fb1e3d7d75ec73fa6550458376311863fa3ee7ea
Павел Сухов <pas402@yandex.ru>
2023-03-18 13:36:13 +0300

    ver 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
-    html_content = '<html><head><title>Анфиса для друзей</title></head><body>'
-    html_content += '<h1>Главная страница</h1>'
-    html_content += '</body></html>'    
-    return HttpResponse(html_content) 
+    # html_content = '<html><head><title>Анфиса для друзей</title></head><body>'
+    # html_content += '<h1>Главная страница отображается из views</h1>'
+    # html_content += '</body></html>'    
+    # return HttpResponse(html_content)
+    template = 'posts/index.html'
+    return render(request, template) 
+
+def group_posts(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
+    # return HttpResponse(f'Мороженое номер {slug}')
